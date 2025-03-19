11fb9819c7fed587edfc80b35118be80f225dd3d
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-19 09:27:58 -0300

    atualização das migrations

--- database/migrations/2025_03_17_135222_enderecos.php
+++ database/migrations/2025_03_17_135222_enderecos.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string("logradouro");
             $table->integer("numero")->nullable();
             $table->string("cidade");
-            $table->foreignId('contato_id')->constrained('contatos');
         });
     }
 
