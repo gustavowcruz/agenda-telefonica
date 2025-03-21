9eb022ad0c51de1461246a0f09bf89b4bc9db9ed
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-21 10:05:54 -0300

    criação das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [ContatoController::class,'index']);
+Route::get('/index', [ContatoController::class,'index']);
+Route::get('/create', [ContatoController::class,'create']);
+Route::get('/show/{id}', [ContatoController::class,'show']);
+Route::get('/edit/{id}', [ContatoController::class,'edit']);
