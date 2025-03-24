1b724628af84b763d9b38ab61844ab7d528b10b2
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-24 11:13:55 -0300

    implementação do name nas rotas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ContatoController::class,'index']);
-Route::get('/index', [ContatoController::class,'index']);
-Route::get('/create', [ContatoController::class,'create']);
-Route::get('/show/{id}', [ContatoController::class,'show']);
-Route::get('/edit/{id}', [ContatoController::class,'edit']);
+Route::get('/index', [ContatoController::class,'index'])->name('contatos.index');
+Route::get('/create', [ContatoController::class,'create'])->name('contatos.create');
+Route::get('/show/{id}', [ContatoController::class,'show'])->name('contatos.show');
+Route::get('/edit/{id}', [ContatoController::class,'edit'])->name('contatos.edit');
+Route::post('/store', [ContatoController::class,'store'])->name('contatos.store');
+Route::patch('/update/{id}', [ContatoController::class,'update'])->name('contatos.update');
+Route::delete('/delete/{id}', [ContatoController::class,'destroy'])->name('contatos.destroy');
