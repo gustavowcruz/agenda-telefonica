e559454027e19103ecf6ae367b3e69a92e419e1c
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-25 11:06:45 -0300

    implementação do método put nas rotas

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Route::get('/create', [ContatoController::class,'create'])->name('contatos.creat
 Route::get('/show/{id}', [ContatoController::class,'show'])->name('contatos.show');
 Route::get('/edit/{id}', [ContatoController::class,'edit'])->name('contatos.edit');
 Route::post('/store', [ContatoController::class,'store'])->name('contatos.store');
-Route::patch('/update/{id}', [ContatoController::class,'update'])->name('contatos.update');
+Route::put('/update/{id}', [ContatoController::class,'update'])->name('contatos.update');
 Route::delete('/delete/{id}', [ContatoController::class,'destroy'])->name('contatos.destroy');
