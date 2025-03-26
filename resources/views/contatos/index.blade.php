98d20888d63924e4616313448821107a60aa8e68
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-26 14:12:27 -0300

    implementação de tabelas na index view

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,9 +1,3 @@
-<?php
-    // dump($contatos);
-    foreach ($contatos as $contato) {
-        echo $contato->nome . '<br>' ;
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +7,29 @@
     <title>Index</title>
 </head>
 <body>
+    <table>
+        <tr>
+            <th> Nome </th>
+            <th> Número </th>
+            <th> Cidade </th>
+            <th> Logradouro </th>
+            <th> Número Res. </th>
+            <th> Ações </th>
+        </tr>
+
+            @foreach ($contatos as $contato)
+                <tr><td> {{$contato->nome}} </td>
+                    <td>{{$contato->telefones->first()->numero}}</td>
+                    <td>{{$contato->enderecos->first()->cidade}}</td>
+                    <td>{{$contato->enderecos->first()->logradouro}}</td>
+                    <td>{{$contato->enderecos->first()->numero}}</td>
+                    <td>
+                        <a href ="{{route('contatos.show', $contato->id)}}">Exibir</a>
+                        <a href="{{route('contatos.edit', $contato->id)}}">Editar</a>
+                    </td>
+
+            @endforeach
+    </table>
     <a href=" {{route('contatos.create')}}"> Criar contato</a>
 </body>
 </html>
