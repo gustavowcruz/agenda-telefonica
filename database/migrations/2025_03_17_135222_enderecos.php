de74ad0f6ec8cbce3a1a018b9d6251bedada7b12
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-26 14:10:13 -0300

    adição do nullable nas migrations

--- database/migrations/2025_03_17_135222_enderecos.php
+++ database/migrations/2025_03_17_135222_enderecos.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create("enderecos", function (Blueprint $table) {
             $table->id();
             $table->string("logradouro");
-            $table->integer("numero")->nullable();
+            $table->string("numero")->nullable();
             $table->string("cidade");
             $table->timestamps();
         });
