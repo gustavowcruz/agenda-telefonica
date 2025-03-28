0ce105d433dda5d1a97b85dda90bef982e210f7e
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-28 09:20:38 -0300

    aplicação de styles e correção de erros na view de listagem

--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -5,31 +5,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        </style>
 </head>
 <body>
     <table>
         <tr>
             <th> Nome </th>
-            <th> Número </th>
+            <th> Telefone </th>
+            <th>Telefone 2</th>
             <th> Cidade </th>
             <th> Logradouro </th>
             <th> Número Res. </th>
+            <th> Categorias </th>
             <th> Ações </th>
         </tr>
 
+        {{-- @dd($contatos->get(7)->telefones->first()->numero) --}}
             @foreach ($contatos as $contato)
                 <tr><td> {{$contato->nome}} </td>
-                    <td>{{$contato->telefones->first()->numero}}</td>
+                    <td>
+                        {{$contato->telefones->first()->numero}}
+                        {{$contato->telefones->first()->tipo}}
+                    </td>
+                    <td>
+                        {{isset($contato->telefones->get(1)->numero) ? $contato->telefones->get(1)->numero : ' '}}
+                        {{isset($contato->telefones->get(1)->tipo_telefone_id) ? $contato->telefones->get(1)->tipo : ' '}}
+
+                    </td>
                     <td>{{$contato->enderecos->first()->cidade}}</td>
                     <td>{{$contato->enderecos->first()->logradouro}}</td>
                     <td>{{$contato->enderecos->first()->numero}}</td>
                     <td>
+                        {{$contato->categorias->pluck('classificacao')->implode(', ')}}
+                    <td>
+
                         <a href ="{{route('contatos.show', $contato->id)}}">Exibir</a>
                         <a href="{{route('contatos.edit', $contato->id)}}">Editar</a>
                     </td>
 
             @endforeach
     </table>
-    <a href=" {{route('contatos.create')}}"> Criar contato</a>
+   <form action="{{route('contatos.create')}}" method="GET">
+        @method('GET')
+        <button type="submit">Novo Contato</button>
+   </form>
 </body>
 </html>
