7153bffae98c2123ae7b36418a77b0764cad75f8
Gustavo Wagner <gwgustavowagner@hotmail.com>
2025-03-28 09:18:28 -0300

    correção do método de busca e comentários no controlador de contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,14 +8,15 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
+
     protected $contatos, $categorias, $tipos_telefones;
     public function __construct(Contato $contatos, Categoria $categorias, TipoTelefone $tipos_telefones)
     {
         $this->telefones = new Telefone();
         $this->enderecos = new Endereco();
         $this->contatos = $contatos;
-        $this->categorias = $categorias->all()->pluck('classificacao', 'id'); //quando você pega as coisas que NÃO é pra chamar e só referenciar você faz isso ai [all()->pluck('algm coisa', 'id')]
-        $this->tipos_telefones = $tipos_telefones->all()->pluck('nome','id'); //ele inverte ;)
+        $this->categorias = $categorias->all()->pluck('classificacao', 'id'); // quando você pega as coisas que NÃO é pra chamar e só referenciar você faz isso ai [all()->pluck('algm coisa', 'id')]
+        $this->tipos_telefones = $tipos_telefones->all()->pluck('nome','id'); // ele inverte (MAS PODE PASSAR 1 SÓ);)
     }
     /**
      * Display a listing of the resource.
@@ -98,13 +99,13 @@ class ContatoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id)//
     {
-        $contato = tap($this->contatos->findOrFail($id))->update([ //tap(): retorna oq ta dentro do tap e executa o q vem dps
+        $contato = tap($this->contatos->findOrFail($id))->update([ //tap(): retorna oq ta dentro do tap e executa o q vem dps, permite encadear metodos
             'nome'=> $request->nome,
         ]);
 
-       tap($this->enderecos->findOrFail($contato->enderecos->first()->id))->update([
+        tap($this->enderecos->findOrFail($contato->enderecos->first()->id))->update([
             'cidade' => $request->cidade,
             'logradouro' => $request->logradouro,
             'numero' => $request->numero_endereco,
@@ -115,9 +116,10 @@ class ContatoController extends Controller
             'contato_id' => $contato->id,
             'tipo_telefone_id' => $request->tipo_telefone1,
         ]);
+
         if (isset($request->telefone2)) {
-            if($contato->telefones->find(1) != null) {
-                tap($this->telefones->findOrFail($request->telefone2->id))->update([
+            if($contato->telefones->get(1) != null) {
+                tap($this->telefones->findOrFail($contato->telefones->get(1)->id))->update([
                     'numero'=> $request->telefone2,
                     'contato_id' => $contato->id,
                     'tipo_telefone_id' => $request->tipo_telefone2,
@@ -133,8 +135,8 @@ class ContatoController extends Controller
             $contato->telefones->get(1)->delete();
         }
 
-        $contato->categorias()->sync($request->categoria_id);
-        return redirect()->route('contatos.index', $contato->id);
+        $contato->categorias()->sync($request->categoria_id); //diferente do atach, que adciona sem remover, ele substitui todas as categorias existentes
+            return redirect()->route('contatos.index');
     }
 
     /**
